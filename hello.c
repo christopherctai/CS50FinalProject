b7ebdfb59ba8abb8eb5993c8f40698d6c01f20d1
christopherctai <christopherctai@users.noreply.github.com>
2022-07-06 05:34:22 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -4,6 +4,9 @@
 int main(void)
 
 {
-    string name = get_string ("What's your name? ");
+    //ask for name
+    string name = get_string("What's your name? ");
+
+    //print hello, name
     printf("hello, %s\n", name);
 }
