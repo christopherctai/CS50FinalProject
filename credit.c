c873f49075902ea57894c3564b5a5abbdcd0cd5d
christopherctai <christopherctai@users.noreply.github.com>
2022-07-10 07:37:51 +0000

    automated commit by submit50

--- credit.c
+++ credit.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 #include <cs50.h>
 
-long checkthesum (long x);
+long checkthesum(long x);
 
-int main (void)
+int main(void)
 {
+    // Define variables answer and checksum
     long answer;
     int checksum;
 
     // Prompt user for credit card number until last digit of checksum is 0
     do
     {
-        answer = get_long ("Number: ");
+        answer = get_long("Number: ");
     }
     while (answer < 0);
 
+    // Set the output of function to long z
     long z = checkthesum(answer);
 
+    // Check whether checksum is 0. If so, print credit card name. If not, print INVALID.
     if (z == 0)
     {
         if ((answer / 10000000000000) % 100 == 34 || (answer / 10000000000000) % 100 == 37)
         {
             printf("AMEX\n");
         }
-        else if ((answer / 100000000000000) % 100 == 51 || (answer / 100000000000000) % 100 == 52 || (answer / 100000000000000) % 100 == 53 || (answer / 100000000000000) % 100 == 54 || (answer / 100000000000000) % 100 == 55)
+        else if ((answer / 100000000000000) % 100 == 51 || (answer / 100000000000000) % 100 == 52 || (answer / 100000000000000) % 100 == 53
+
+                 || (answer / 100000000000000) % 100 == 54 || (answer / 100000000000000) % 100 == 55)
         {
             printf("MASTERCARD\n");
         }
@@ -43,8 +48,9 @@ int main (void)
 }
 
 // Define function that returns the last digit of the checksum
-long checkthesum (long x)
+long checkthesum(long x)
 {
+    // Find individual sums
     int checksum;
     int n = 0;
     int m = 0;
@@ -57,7 +63,7 @@ long checkthesum (long x)
         long secondtolastdigit = newanswer % 10;
         long newnewanswer = (newanswer - secondtolastdigit) / 10;
         n = (2 * secondtolastdigit);
-        o = ((n % 10) + (n - (n % 10))/10) + o;
+        o = ((n % 10) + (n - (n % 10)) / 10) + o;
         m = lastdigit + m;
         x = newnewanswer;
     }
