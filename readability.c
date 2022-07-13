8222b5210c3c0c829ea5a134dfb5a4ce008d4d21
christopherctai <christopherctai@users.noreply.github.com>
2022-07-13 00:01:18 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -9,9 +9,12 @@ float averagesentencenumber(string S);
 
 int main(void)
 {
+    // Get text and use functions to calculate the Coleman-Liau index of text
     string text = get_string("Text: ");
     float index = 0.0588 * averagewordlength(text) - 0.296 * averagesentencenumber(text) - 15.8;
     int x = round(index);
+
+    // Print the corresponding Grade number
     if (x > 15)
     {
         printf("Grade 16+\n");
@@ -26,11 +29,13 @@ int main(void)
     }
 }
 
+// Define function that calculates the average letters per 100 words
 float averagewordlength(string L)
 {
     float numberofwords = 1;
     float numberofletters = 0;
 
+    // Run loop to count number of words
     for (int i = 0, n = strlen(L); i < n; i++)
     {
         if (L[i] == ' ')
@@ -39,6 +44,7 @@ float averagewordlength(string L)
         }
     }
 
+    // Run loop to count number of letters
     for (int j = 0, n = strlen(L); j < n; j++)
     {
         if (isupper(L[j]) || islower(L[j]))
@@ -47,17 +53,19 @@ float averagewordlength(string L)
         }
     }
 
+    // Calculate average number of letters per 100 words
     float averageletters = (numberofletters / numberofwords) * 100.0;
 
     return averageletters;
 }
 
-
+// Define function that calculates the average sentences per 100 words
 float averagesentencenumber(string S)
 {
     float numberofwords = 1;
     float numberofsentences = 0;
 
+    // Run loop to count the number of words
     for (int i = 0, n = strlen(S); i < n; i++)
     {
         if (S[i] == ' ')
@@ -66,6 +74,7 @@ float averagesentencenumber(string S)
         }
     }
 
+    // Run loop to count the number of sentences
     for (int j = 0, n = strlen(S); j < n; j++)
     {
         if (S[j] == '.' || S[j] == '!' || S[j] == '?')
@@ -74,6 +83,7 @@ float averagesentencenumber(string S)
         }
     }
 
+    // Calculate the average sentences per 100 words
     float averagesentences = (numberofsentences / numberofwords) * 100;
 
     return averagesentences;
