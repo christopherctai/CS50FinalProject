916bd78f959e55f3d20e5132423410a1edcd6dea
christopherctai <christopherctai@users.noreply.github.com>
2022-08-03 20:03:28 +0000

    automated commit by submit50

--- volume.c
+++ volume.c
@@ -35,6 +35,7 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
 
+    // Here we are copying one massive chunk of data. We copy the massive HEADER_SIZE 44-byte long chunk all at once! 
     uint8_t *header_array = malloc(sizeof(uint8_t) * HEADER_SIZE);
     fread(header_array, HEADER_SIZE, 1, input);
     fwrite(header_array, HEADER_SIZE, 1, output);
@@ -44,6 +45,10 @@ int main(int argc, char *argv[])
 
     int16_t buffer;
 
+    // Read the buffer one at a time
+    // Important - every time one of the sizeof(int16_t) is read, the &buffer address changes to the next byte over
+    // So, we can read first, then change that specific value at that byte to multiply it times the factor, then write it into the output file
+    // This continues until fread is 0; if it is 0, that means it read 0 bytes and therefore it's at EOF
     while (fread(&buffer, sizeof(int16_t), 1, input) > 0)
     {
         buffer *= factor;
