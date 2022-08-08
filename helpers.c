6e8e244d8cab15f052779de25bc91ad37934bb8e
christopherctai <christopherctai@users.noreply.github.com>
2022-08-08 04:46:26 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -60,7 +60,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+
     // Define arrays to store the color values so you don't change them as they go
     double redvalues[height][width];
     double greenvalues[height][width];
@@ -256,5 +256,242 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Define arrays to store the color values so you don't change them as they go
+    double redvalues[height][width];
+    double greenvalues[height][width];
+    double bluevalues[height][width];
+
+    // Define arrays to store Gx and Gy kernels
+    double Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    double Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+    // Test Print
+    /*
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%f\n", Gx[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%f\n", Gy[i][j]);
+        }
+    }
+    */
+
+
+    // Populate the new color arrays so you have static arrays
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            redvalues [i][j] = image[i][j].rgbtRed;
+            greenvalues [i][j] = image[i][j].rgbtGreen;
+            bluevalues [i][j] = image[i][j].rgbtBlue;
+        }
+    }
+
+
+    // Iterate over each pixel one at a time
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Counts the number of pixels around the pixel in question. Resets for each pixel
+            double pixelcount = 0;
+
+            // Define Gx and Gy for RGB. Resets for each pixel
+            double Gxred = 0;
+            double Gyred = 0;
+            double Gxgreen = 0;
+            double Gygreen = 0;
+            double Gxblue = 0;
+            double Gyblue = 0;
+
+            /*
+
+            // If top row
+            if (i == 0 && j != 0 && j != width - 1)
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    for (int l = -1; l < 2; l++)
+                    {
+                        for (int m = -1; m < 2)
+
+
+                        redvalues[i + k][j + l];
+                        totalgreen += greenvalues[i + k][j + l];
+                        totalblue += bluevalues[i + k][j + l];
+
+                        pixelcount++;
+                    }
+                }
+            }
+
+            // If left row
+            else if (j == 0 && i != 0 && i != height - 1)
+            {
+                for (int k = -1; k < 2; k++)
+                {
+                    for (int l = 0; l < 2; l++)
+                    {
+                        totalred += redvalues[i + k][j + l];
+                        totalgreen += greenvalues[i + k][j + l];
+                        totalblue += bluevalues[i + k][j + l];
+
+                        pixelcount++;
+                    }
+                }
+            }
+
+            // If right row
+            else if (j == (width - 1) && i != 0 && i != height - 1)
+            {
+                for (int k = -1; k < 2; k++)
+                {
+                    for (int l = -1; l < 1; l++)
+                    {
+                        totalred += redvalues[i + k][j + l];
+                        totalgreen += greenvalues[i + k][j + l];
+                        totalblue += bluevalues[i + k][j + l];
+
+                        pixelcount++;
+                    }
+                }
+            }
+
+            // If bottom row
+            else if (i == (height - 1) && j != 0 && j != width - 1)
+            {
+                for (int k = -1; k < 1; k++)
+                {
+                    for (int l = -1; l < 2; l++)
+                    {
+                        totalred += redvalues[i + k][j + l];
+                        totalgreen += greenvalues[i + k][j + l];
+                        totalblue += bluevalues[i + k][j + l];
+
+                        pixelcount++;
+                    }
+                }
+            }
+
+            // If top left corner
+            else if (i == 0 && j == 0)
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    for (int l = 0; l < 2; l++)
+                    {
+                        totalred += redvalues[i + k][j + l];
+                        totalgreen += greenvalues[i + k][j + l];
+                        totalblue += bluevalues[i + k][j + l];
+
+                        pixelcount++;
+                    }
+                }
+            }
+
+            // If top right corner
+            else if (i == 0 && j == (width - 1))
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    for (int l = -1; l < 1; l++)
+                    {
+                        totalred += redvalues[i + k][j + l];
+                        totalgreen += greenvalues[i + k][j + l];
+                        totalblue += bluevalues[i + k][j + l];
+
+                        pixelcount++;
+                    }
+                }
+            }
+
+            // If bottom left corner
+            else if (i == (height - 1) && j == 0)
+            {
+                for (int k = -1; k < 1; k++)
+                {
+                    for (int l = 0; l < 2; l++)
+                    {
+                        totalred += redvalues[i + k][j + l];
+                        totalgreen += greenvalues[i + k][j + l];
+                        totalblue += bluevalues[i + k][j + l];
+
+                        pixelcount++;
+                    }
+                }
+            }
+
+            // If bottom right corner
+            else if (i == (height - 1) && j == (width - 1))
+            {
+                for (int k = -1; k < 1; k++)
+                {
+                    for (int l = -1; l < 1; l++)
+                    {
+                        totalred += redvalues[i + k][j + l];
+                        totalgreen += greenvalues[i + k][j + l];
+                        totalblue += bluevalues[i + k][j + l];
+
+                        pixelcount++;
+                    }
+                }
+            }
+
+            */
+
+            // If not an edge or corner case. Iterates all around the current pixel.
+            /* else */
+            if (i != 0 && i != height - 1 && j != 0 && j != width - 1)
+            {
+                for (int k = -1; k < 2; k++)
+                {
+                    for (int l = -1; l < 2; l++)
+                    {
+                        Gxred += redvalues[i + k][j + l] * Gx[k + 1][l + 1];
+                        Gyred += redvalues[i + k][j + l] * Gy[k + 1][l + 1];
+                        Gxgreen += greenvalues[i + k][j + l] * Gx[k + 1][l + 1];
+                        Gygreen += greenvalues[i + k][j + l] * Gy[k + 1][l + 1];
+                        Gxblue += bluevalues[i + k][j + l] * Gx[k + 1][l + 1];
+                        Gyblue += bluevalues[i + k][j + l] * Gy[k + 1][l + 1];
+                    }
+                }
+            }
+
+            // Calculate Sobel values for RGB
+            double sobelred = round(sqrt((Gxred * Gxred) + (Gyred * Gyred)));
+            double sobelgreen = round(sqrt((Gxgreen * Gxgreen) + (Gygreen * Gygreen)));
+            double sobelblue = round(sqrt((Gxblue * Gxblue + Gyblue * Gyblue)));
+
+            if (sobelred > 255)
+            {
+                sobelred = 255;
+            }
+
+            if (sobelgreen > 255)
+            {
+                sobelgreen = 255;
+            }
+
+            if (sobelblue > 255)
+            {
+                sobelblue = 255;
+            }
+
+            image[i][j].rgbtRed = sobelred;
+            image[i][j].rgbtGreen = sobelgreen;
+            image[i][j].rgbtBlue = sobelblue;
+        }
+
+    }
+
     return;
 }
