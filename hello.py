91b6686195f40a048a06b5f7675bb25cc3128c8e
christopherctai <christopherctai@users.noreply.github.com>
2022-08-23 04:38:45 +0000

    automated commit by submit50

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 # TODO
 
+# Import the correct function from cs50. We want to get the user's input for their name
 from cs50 import get_string
 
+# Get the user's name, then print a string where it's Hello and then their name 
 name = get_string("What's your name? ")
 print(f"Hello, {name}")
