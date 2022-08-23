4fb74940680f68c8e1a736d6ea9a5ceb703c3fcf
christopherctai <christopherctai@users.noreply.github.com>
2022-08-23 05:54:33 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -3,12 +3,17 @@
 from cs50 import get_int
 
 # Get input in a do-while loop except it's Python
-
 while True:
     height = int(get_int("Height: "))
     if height < 9 and height > 0:
         break
 
+# Do something height number of times.
+# First, print out the most amount of spaces
+# Then, print out the number of hashes for the first pyramid
+# Then, print out "  " to create the gap in between the pyramids
+# Then, print out the number of hashes for the second pyramid
+# Then, print an extra line (\n) at the end for the next row
 for i in (range(1, height + 1)):
     print(" " * (height - i), end="")
     print("#" * i, end="")
