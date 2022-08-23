7a03596f27073fd4d7625879bea379eebac88cfe
christopherctai <christopherctai@users.noreply.github.com>
2022-08-23 04:28:58 +0000

    automated commit by check50 [check50=True]

--- tournament.py
+++ tournament.py
@@ -23,40 +23,16 @@ def main():
             teams.append(row)
 
     # Define a dictionary counts
-    counts = {
-        "Uruguay": 0,
-        "Portugal": 0,
-        "France": 0,
-        "Argentina": 0,
-        "Brazil": 0,
-        "Mexico": 0,
-        "Belgium": 0,
-        "Japan": 0,
-        "Spain": 0,
-        "Russia": 0,
-        "Croatia": 0,
-        "Denmark": 0,
-        "Sweden": 0,
-        "Switzerland": 0,
-        "Colombia": 0,
-        "England": 0,
-        "Norway": 0,
-        "Australia": 0,
-        "Cameroon": 0,
-        "United States": 0,
-        "Italy": 0,
-        "China PR": 0,
-        "Netherlands": 0,
-        "Germany": 0,
-        "Nigeria": 0,
-        "Canada": 0
-    }
+    counts = {}
 
 
     # TODO: Simulate N tournaments and keep track of win counts
     for i in range(N):
         winner = simulate_tournament(teams)
-        counts[winner] += 1
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
