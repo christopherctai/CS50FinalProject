7cbde4a8bfed8e0a3307b983f4a721f78d88ef96
christopherctai <christopherctai@users.noreply.github.com>
2022-09-24 19:08:43 +0000

    automated commit by check50 [check50=True]

--- 11.sql
+++ 11.sql
@@ -1,6 +1,3 @@
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE movie_id IN
-(SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Chadwick Boseman"))) ORDER BY title DESC LIMIT 5;
-
 SELECT title FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
@@ -8,4 +5,4 @@ JOIN ratings ON movies.id = ratings.movie_id
 WHERE name = "Chadwick Boseman"
 ORDER by rating
 DESC
-LIMIT 10;
+LIMIT 5;
