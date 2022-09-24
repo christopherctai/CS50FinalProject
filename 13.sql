1c858672ecca922aa6147929068dcce583071141
christopherctai <christopherctai@users.noreply.github.com>
2022-09-24 19:48:16 +0000

    automated commit by check50 [check50=True]

---
+++ 13.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT name FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.id IN (SELECT movie_id FROM stars WHERE person_id =
+(SELECT id FROM people WHERE name = "Kevin Bacon" and birth = "1958"))
+AND NOT name = "Kevin Bacon";
