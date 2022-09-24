1e86eae104465f01f4f44a9cda0fc00e03c3d60d
christopherctai <christopherctai@users.noreply.github.com>
2022-09-24 07:52:14 +0000

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -1,6 +1,10 @@
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Helena Bonham Carter") AND movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Johnny Depp"));
+SELECT title FROM movies
+WHERE id IN
+(SELECT movie_id
+FROM stars
+WHERE person_id = (SELECT id FROM people WHERE name = "Johnny Depp")
+OR person_id = (SELECT id FROM people WHERE name = "Helena Bonham Carter")
+GROUP BY movie_id
+HAVING COUNT(movie_id) = 2);
 
-SELECT COUNT(*) title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE name IN ("Johnny Depp", "Helena Bonham Carter");
-
-SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name IN ("Johnny Depp", "Helena Bonham Carter"));
 
