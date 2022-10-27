4a8605fed27ccbcb049bdebcfa1cbe18e049f4e3
Chris Tai <christopherctai@gmail.com>
2022-10-26 19:52:20 -0700

    Practicing how to use commit with README and app.js

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Final Project for CS50
 
 
 This is my first ever repo on GitHub! Yay! 
+
+# Making more changes here to check a status report 
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log(msg)
 
 console.log('Use alt+click to put your cursor in multiple places at the same time!')
 
-
+// I'm making a new change here! 
 
 console.log('Use alt+click to put your cursor in multiple places at the same time!')
 
